5e5f860b258b0683f931ffe8305e3eb0d6b3789c
Kai Itokazu <56910039+kitokazu@users.noreply.github.com>
2023-02-13 20:50:21 -0800

    Removed bad test

--- pages/__tests__/functions.test.js
+++ pages/__tests__/functions.test.js
@@ -5,7 +5,4 @@ describe('sum module', () => {
   test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
   });
-  test('1 + 2 = 5 (TEST SUPPOSED TO FAIL)', () => {
-    expect(sum(1, 2)).toBe(5);
-  });
 });
