8dc00707ba90a7371fa97bbcc96768a9d9ba8024
Kai Itokazu <56910039+kitokazu@users.noreply.github.com>
2023-02-15 16:26:13 -0800

    deleted babel config file

--- .next/static/webpack/webpack.1425cb7b438d0670.hot-update.js
+++ .next/static/webpack/webpack.1425cb7b438d0670.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "87a1dbe49a7b9b85"; }
+/******/ 	__webpack_require__.h = function() { return "a2cd79b786b3dac5"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
