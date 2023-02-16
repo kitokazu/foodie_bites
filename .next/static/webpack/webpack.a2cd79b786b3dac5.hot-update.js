188118ff98188f0c67f500dbd249dd299811fdcc
Kai Itokazu <56910039+kitokazu@users.noreply.github.com>
2023-02-15 17:33:39 -0800

    Fixed Tests so that is uses next compiler rather than babel

--- .next/static/webpack/webpack.a2cd79b786b3dac5.hot-update.js
+++ .next/static/webpack/webpack.a2cd79b786b3dac5.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "259256acd32a4034"; }
+/******/ 	__webpack_require__.h = function() { return "b709f8a2968c72b7"; }
 /******/ }();
 /******/ 
 /******/ }
