0047d78d22b51ce1760570183e091ca0f969bfa2
Kai Itokazu <56910039+kitokazu@users.noreply.github.com>
2023-02-21 13:26:58 -0800

    Added mesage icon

--- components/Header.js
+++ components/Header.js
@@ -5,12 +5,13 @@ import {
   MagnifyingGlassIcon,
   HomeIcon,
   UserCircleIcon,
-  Bars3Icon
+  Bars3Icon,
+  EnvelopeIcon
 } from "@heroicons/react/24/outline";
 
 function Header() {
   return (
-    <div>
+    <div className="shadow-sm border-b bg-white sticky top-0 z-50">
       <div className="flex justify-between bg-white max-w-6xl mx-5 xl:mx-auto">
         {/* Left Side */}
         <div className="relative hidden lg:inline w-36 h-12 cursor-pointer">
@@ -30,8 +31,9 @@ function Header() {
             style={{objectFit: "contain"}}
             />
         </div>
-        {/* Center */}
-        <div className="relative mt-1 p-3 rounded-md">
+
+        {/* Center --- SEARCH BAR */}
+        <div className="relative mt-3 p-3 rounded-md">
           <div className="mt-2 absolute inset y-0 pl-3 flex items-center pointer-events-none">
             <MagnifyingGlassIcon className="h-5 w-5 text-gray-500"/>
           </div>
@@ -42,6 +44,10 @@ function Header() {
         <div className="flex items-center justify-end space-x-4">
           <HomeIcon className="navBtn"/>
           <UserCircleIcon className="navBtn"/>
+          <div className="relative navBtn">
+            <EnvelopeIcon className="navBtn"/>
+            <div className="absolute -top-2 -right-2 text-s w-5 h-5 bg-red-500 rounded-full flex items-center justify-center animate-pulse text-white">3</div>
+          </div>
           <Bars3Icon className="h-6 md:hidden cursor-pointer"/>
         </div>
 
