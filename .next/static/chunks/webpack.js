f711f71f0c1f1bd462ccd541f9be2022e78bd888
Kai Itokazu <56910039+kitokazu@users.noreply.github.com>
2023-02-20 23:52:57 -0800

    browswer and icons scale correctly

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ded7e8fe6feae6fd"; }
+/******/ 		__webpack_require__.h = function() { return "a34ec80449827784"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
