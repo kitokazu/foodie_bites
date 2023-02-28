306b6625b0d073c210ef8f7ce6c69696a0c91358
rrezvani <rrezvani@chapman.edu>
2023-02-27 20:46:34 -0800

    adding dependency

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { SessionProvider } from 'next-auth/react'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
