50b4a1979f9eab7908b2cb56c85a09e3420b5ba1
Kai Itokazu <56910039+kitokazu@users.noreply.github.com>
2023-02-27 20:02:50 -0800

    Profie Pic and name

--- components/Profile.js
+++ components/Profile.js
@@ -2,26 +2,34 @@ import Image from "next/image";
 import {
 } from "@heroicons/react/24/outline";
 import McDonalds_Logo from '../images/mcdonalds_logo.png';
+import profilePic from '../images/headshot.jpeg';
 
 import { useRouter } from 'next/router'
 
+
 function ProfilePage() {
   return (
-    <>
-      <div className="fixed top-0 h-screen w-72 m-0 flex flex-col bg-gray-100 shadow-lg">
-        
-         {/* Add "Profile" Text */}
-          <i>A</i>
-          <i>B</i>
-          {/* Picture */}
-          {/* <Image src={McDonalds_Logo}
-            alt="McDonalds Logo"
-            fill="contain"
-            style={{objectFit: "contain"}}
-          /> */}
+    <div className="max-w-6xl mx-5 p-10 xl:mx:auto">
+      <div className = "grid grid-cols-4 gap-4">
+        {/* Header for Profile */}
+        <div className="col-span-4">LUKE DRISCOLL</div>
+        <div className="avatar">
+          <div classname = "rounded-full w-36 h-36">
+            <Image src={profilePic}/>
+          </div>
+        </div>
       </div>
-    </>
-  );
+    </div>
+  )
 }
+
+// function ProfilePage() {
+//   return (
+//     <>
+//       <div className="fixed top-0 h-screen w-72 m-0 flex flex-col bg-gray-100 shadow-lg">
+//       </div>
+//     </>
+//   );
+// }
 
 export default ProfilePage
