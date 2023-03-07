3470d5058b261718b2c22504e1634c18b7f40bef
Kai Itokazu <56910039+kitokazu@users.noreply.github.com>
2023-03-06 16:58:03 -0800

    top right routes home

--- components/Header.js
+++ components/Header.js
@@ -39,7 +39,7 @@ function Header() {
     <div className="shadow-sm border-b bg-white sticky top-0 z-50">
       <div className="flex justify-between bg-white max-w-6xl mx-5 xl:mx-auto">
         {/* Left Side */}
-        <div className="relative hidden lg:inline w-36 h-12 cursor-pointer">
+        <div className="relative hidden lg:inline w-36 h-12 cursor-pointer" onClick={routeHomePage}>
           {/* <Image 
             src={McDonalds_Long}
             alt="McDonalds Logo"
