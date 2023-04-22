d75ba5da990c1563464b63b622b3389fa9a60954
Kai Itokazu <56910039+kitokazu@users.noreply.github.com>
2023-04-22 14:19:38 -0700

    updated tests

--- pages/__tests__/components.test.js
+++ pages/__tests__/components.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 // import "@#testing-library/jest-dom/";
-import Dashboard from "@/pages/dashboard";
+import Dashboard from "../dashboard";
 
 describe("dashboard", () => {
   it("renders correctly", () => {
