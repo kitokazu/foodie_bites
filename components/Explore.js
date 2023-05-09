47adb28032fee67cb2d84a5a0a97494c5f053a68
rrezvani <rrezvani@chapman.edu>
2023-05-08 20:27:05 -0700

    adding stuff

--- components/Explore.js
+++ components/Explore.js
@@ -1,17 +1,24 @@
-import Posts from './Posts'
+import React, { useEffect, useState, useRef } from 'react'
 
-function Home() {
+const apiKey = process.env.GOOGLE_PLACES_API_KEY
+const mapApiJs = 'https://maps.googleapis.com/maps/api/js'
+const geocodeJson = 'https://maps.googleapis.com/maps/api/geocode/json'
+
+
+function Explore() {
   return (
     <div className="bg-gray-50 h-screen">
-      <main
-        className="grid grid-colos-1 md:grid-cols-2 md:max-w-3xl
+        <div>
+          </div>
+          <div
+            className="grid grid-colos-1 md:grid-cols-2 md:max-w-3xl
       xl:grid-cols-3 xl:max-w-6xl mx-auto bg-gray-50"
-      >
-        <section className="col-span-2 bg-gray-50"></section>
-        <section></section>
-      </main>
+          >
+            <section className="col-span-2 bg-gray-50"></section>
+            <section></section>
+          </div>{' '}
     </div>
   )
 }
 
-export default Home
+export default Explore
