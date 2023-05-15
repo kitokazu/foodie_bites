45872292b3fb3ed1107da42c02bbb3ec3ffe454a
Kai Itokazu <56910039+kitokazu@users.noreply.github.com>
2023-05-15 15:43:33 -0700

    Added length of reviews to profile

--- components/ProfileSidebar.js
+++ components/ProfileSidebar.js
@@ -14,6 +14,12 @@ import {
 import { db } from '../firebase'
 import Moment from 'react-moment'
 
+import amazing from '../public/images/amazing.png'
+import awful from '../public/images/awful.png'
+import meh from '../public/images/meh.png'
+import okay from '../public/images/okay.png'
+import great from '../public/images/great.png'
+
 function ProfileSidebar({ username, image }) {
   const { data: session } = useSession()
   const [posts, setPosts] = useState([])
@@ -44,6 +50,41 @@ function ProfileSidebar({ username, image }) {
     }
   }, [db, session, username])
 
+  // Get the length of the posts that are ranked 5
+  const fiveStarPosts = posts.filter((post) => post.data().rating == 5)
+  const fiveStarPostsLength = fiveStarPosts.length
+
+  // Get the length of the posts that are ranked 4
+  const fourStarPosts = posts.filter((post) => post.data().rating == 4)
+  const fourStarPostsLength = fourStarPosts.length
+
+  // Get the length of the posts that are ranked 3
+  const threeStarPosts = posts.filter((post) => post.data().rating == 3)
+  const threeStarPostsLength = threeStarPosts.length
+
+  // Get the length of the posts that are ranked 2
+  const twoStarPosts = posts.filter((post) => post.data().rating == 2)
+  const twoStarPostsLength = twoStarPosts.length
+
+  // Get the length of the posts that are ranked 1
+  const oneStarPosts = posts.filter((post) => post.data().rating == 1)
+  const oneStarPostsLength = oneStarPosts.length
+
+  const reviewColor = (rating) => {
+    if (Math.floor(rating) == 1 || Math.floor(rating) == 0) {
+      return 'text-awful'
+    } else if (Math.floor(rating) == 2) {
+      return 'text-meh'
+    } else if (Math.floor(rating) == 3) {
+      return 'text-okay'
+    } else if (Math.floor(rating) == 4) {
+      return 'text-great'
+    } else if (Math.floor(rating) == 5) {
+      return 'text-amazing'
+    }
+    return 'text-gray-500'
+  }
+
   return (
     <div className="w-1/4 bg-gray-200 h-screen">
       <div className="avatar justify-center ml-10">
@@ -62,13 +103,72 @@ function ProfileSidebar({ username, image }) {
           </span>
           <Cog6ToothIcon className="cursor-pointer inline w-8 h-8 text-gray-700 rounded mr-4 mb-2" />
           <div className="mt-1 flex flex-col">
-            <div className="text-yellow-600">Active Foodie</div>
-            <div className="mt-4">
+            <div className="mt-4 text-xl">
               Reviews:{' '}
               <span className="font-semibold ml-1">{posts.length}</span>
             </div>
-            <div className="mt-4">
-              Favorite Places: <span className="font-semibold ml-1">Canes</span>
+            {/** RATINGS AND COUNTS **/}
+            <div className="mt-4 ml-3">
+              <div className="w-16 h-16 flex">
+                <Image
+                  src={amazing}
+                  width={500}
+                  height={500}
+                  alt="IMAGE NOT FOUND"
+                  className="object-cover w-full h-full "
+                />
+                <p className="text-4xl ml-3 mt-3 font-sans font-bold text-amazing">
+                  {fiveStarPostsLength}
+                </p>
+              </div>
+              <div className="mt-5 w-16 h-16 flex">
+                <Image
+                  src={great}
+                  width={500}
+                  height={500}
+                  alt="IMAGE NOT FOUND"
+                  className="object-cover w-full h-full "
+                />
+                <p className="text-4xl ml-3 mt-3 font-sans font-bold text-great">
+                  {fourStarPostsLength}
+                </p>
+              </div>
+              <div className="mt-5 w-16 h-16 flex">
+                <Image
+                  src={okay}
+                  width={500}
+                  height={500}
+                  alt="IMAGE NOT FOUND"
+                  className="object-cover w-full h-full "
+                />
+                <p className="text-4xl ml-3 mt-3 font-sans font-bold text-okay">
+                  {threeStarPostsLength}
+                </p>
+              </div>
+              <div className="mt-5 w-16 h-16 flex">
+                <Image
+                  src={meh}
+                  width={500}
+                  height={500}
+                  alt="IMAGE NOT FOUND"
+                  className="object-cover w-full h-full "
+                />
+                <p className="text-4xl ml-3 mt-3 font-sans font-bold text-meh">
+                  {twoStarPostsLength}
+                </p>
+              </div>
+              <div className="mt-5 w-16 h-16 flex">
+                <Image
+                  src={awful}
+                  width={500}
+                  height={500}
+                  alt="IMAGE NOT FOUND"
+                  className="object-cover w-full h-full "
+                />
+                <p className="text-4xl ml-3 mt-3 font-sans font-bold text-awful">
+                  {oneStarPostsLength}
+                </p>
+              </div>
             </div>
           </div>
         </div>
